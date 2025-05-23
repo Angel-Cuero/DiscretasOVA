e72950d7e89b5837cbea6d53ea87484a5c901700
Santiago-Ospina-Gonzalez <angel.cuero01@uceva.edu.co>
2025-05-22 20:59:08 -0500

    Creacion del microservicio para la libreria de discretas

---
+++ src/main/java/co/edu/uceva/discretasjni/DiscretasJniApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.discretasjni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscretasJniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscretasJniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uceva/discretasjni/DiscretasJniApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.discretasjni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscretasJniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
