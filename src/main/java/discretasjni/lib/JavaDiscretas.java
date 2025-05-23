486f850fba638874a85b9eebbbd9267191ed81e0
Santiago-Ospina-Gonzalez <angel.cuero01@uceva.edu.co>
2025-05-22 22:01:22 -0500

    logica de la clase JavaDiscretas

--- src/main/java/discretasjni/lib/JavaDiscretas.java
+++ src/main/java/discretasjni/lib/JavaDiscretas.java
@@ -1,4 +1,18 @@
 package discretasjni.lib;
 
 public class JavaDiscretas {
+
+    static { System.loadLibrary("discretas"); }
+
+    private native int[] unionSets(int[] a, int[] b);
+    private native int[] intersectionSets(int[] a, int[] b);
+    private native int[] differenceSets(int[] a, int[] b);
+    private native int[] symmetricDifferenceSets(int[] a, int[] b);
+    private native int[] complementSet(int[] universal, int[] a);
+
+    public int[] union(int[] a, int[] b) { return unionSets(a, b); }
+    public int[] intersection(int[] a, int[] b) { return intersectionSets(a, b); }
+    public int[] difference(int[] a, int[] b) { return differenceSets(a, b); }
+    public int[] symmetricDifference(int[] a, int[] b) { return symmetricDifferenceSets(a, b); }
+    public int[] complement(int[] universal, int[] a) { return complementSet(universal, a); }
 }
